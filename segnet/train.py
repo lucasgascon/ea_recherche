2f5297fb3eeeb35d45c0b9549585acf0429ec652
lucasgascon <76687633+lucasgascon@users.noreply.github.com>
2022-11-08 14:47:01 +0100

    commit

--- segnet/train.py
+++ segnet/train.py
@@ -2,13 +2,13 @@
 
 from keras_segmentation.models.segnet import resnet50_segnet
 
-def custom_augmentation():
-    return  iaa.Sequential(
-        [
-            # apply the following augmenters to most images
-            iaa.Fliplr(0.5),  # horizontally flip 50% of all images
-            iaa.Flipud(0.5), # horizontally flip 50% of all images
-        ])
+# def custom_augmentation():
+#     return  iaa.Sequential(
+#         [
+#             # apply the following augmenters to most images
+#             iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+#             iaa.Flipud(0.5), # horizontally flip 50% of all images
+#         ])
 
 
 def train (model, n_classes, train_images, train_annotations, val_images, val_annotations,
@@ -23,36 +23,36 @@ def train (model, n_classes, train_images, train_annotations, val_images, val_an
         batch_size = batch_size,
         val_batch_size = batch_size,
 
-        # steps_per_epoch= 20210 // batch_size,
-        # val_steps_per_epoch= 2000 // batch_size,
+        steps_per_epoch= 20210 // batch_size,
+        val_steps_per_epoch= 2000 // batch_size,
 
         checkpoints_path =  'tmp/' + name_dataset + '/' + name_model + '/', 
         epochs= epochs,
 
-        do_augment=True, # enable augmentation 
-        custom_augmentation=custom_augmentation # sets the augmention function to use
+        # do_augment=True, # enable augmentation 
+        # custom_augmentation=custom_augmentation # sets the augmention function to use
     )
 
     model.save_weights("pretrained/" + name_dataset + '/' + name_model + '_' + str(n_classes) + '/' )
 
 #%%
 
-n_classes = 51
+n_classes = 200
 model = resnet50_segnet(n_classes=n_classes ,  input_height=416, input_width=608 ) 
 
-# train_images = '../datasets/ADEChallengeData2016/images/training/'
-# train_annotations = '../datasets/ADEChallengeData2016/annotations/training/'
-# val_images = '../datasets/ADEChallengeData2016/images/validation/'
-# val_annotations = '../datasets/ADEChallengeData2016/images/validation/'
+train_images = '../datasets/ADEChallengeData2016/images/training/'
+train_annotations = '../datasets/ADEChallengeData2016/annotations/training/'
+val_images = '../datasets/ADEChallengeData2016/images/validation/'
+val_annotations = '../datasets/ADEChallengeData2016/images/validation/'
 
-train_images = '../datasets/dataset1/images_prepped_train/'
-train_annotations = '../datasets/dataset1/annotations_prepped_train/'
-val_images = None
-val_annotations = None
+# train_images = '../datasets/dataset1/images_prepped_train/'
+# train_annotations = '../datasets/dataset1/annotations_prepped_train/'
+# val_images = None
+# val_annotations = None
 
 name_dataset = 'dataset1'
 name_model = 'resnet50_segnet'
-batch_size = 2
+batch_size = 10
 epochs = 10
 
 #%%
