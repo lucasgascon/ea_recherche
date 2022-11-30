7c1442b30393066bb515b9b776742df79185f058
lucasgascon <76687633+lucasgascon@users.noreply.github.com>
2022-11-30 15:33:27 +0100

    commit

--- segnet/utils.py
+++ segnet/utils.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt 
 from PIL import Image as im
+import cv2
 
-import os
+import os 
 import random
 
-def infer_dir(model, dir_path, n):
+def infer_dir(model, dir_path, n, k = None):
     list_img = os.listdir(dir_path)[:n]
     random.shuffle(list_img)
     for img in list_img:
@@ -15,9 +16,18 @@ def infer_dir(model, dir_path, n):
             # out_fname="out.png"
         )
 
-        plt.imshow(im.open(img_path))
-        plt.show()
-        plt.imshow(out)
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3.2),
+                                        # sharex=True, sharey=True,
+                                        )
+        ax1.imshow(cv2.imread(img_path))
+        ax1.axis('off')
+        ax1.set_title('Image')
+        ax2.imshow(out, cmap='gray')
+        ax2.axis('off')
+        ax2.set_title('Prédiction')
+        fig.suptitle('Exemple de prédiction de PSPNet ré-entraîné sur ' + str(k) + ' images.')
+        fig.tight_layout()
         plt.show()
 
 def infer_img(model, img_path):
